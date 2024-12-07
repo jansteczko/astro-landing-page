2ee5d15a6b0ec1be75a1667aef7bd0945183df00
janek <jansteczko@MacBook-Pro-Jan.local>
2024-12-07 10:18:50 +0100

    innitial commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	content: [
+	  // './src/**/*.{html,js}',
+		'node_modules/preline/dist/*.js',
+	],
+	plugins: [
+	  // require('@tailwindcss/forms'),
+		require('preline/plugin'),
+	], }
+module.exports = {
+	content: ["./node_modules/flyonui/dist/js/*.js"], // Require only if you want to use FlyonUI JS component
+
+  plugins: [
+    require("flyonui"),
+    require("flyonui/plugin") // Require only if you want to use FlyonUI JS component
+  ],
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
